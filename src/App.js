5872258c6f4f081f7f81b6d118053f0d88974e00
Vinyas Singh <kumar.v.singh@oracle.com>
2020-07-26 00:22:36 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,88 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Button, CircularProgress } from "@material-ui/core";
+import { ArrowBack, ArrowForward } from "@material-ui/icons";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import NewsFilter from "./components/NewsFilter/NewsFilter";
+import NewsDisplay from "./components/NewsDisplay/NewsDisplay";
+import { fetchNews } from "./api/news";
+
+import "./App.css";
+
+class App extends Component {
+  state = {
+    articles: [],
+    currentPage: 1,
+  };
+
+  async componentDidMount() {
+    document.addEventListener(
+      "keydown",
+      (e) => {
+        if (
+          e.keyCode === 39 &&
+          this.state.currentPage !== this.state.articles.length
+        ) {
+          this.next();
+        }
+
+        if (e.keyCode === 37 && this.state.currentPage !== 0) {
+          this.prev();
+        }
+      },
+      false
+    );
+    const articles = await fetchNews();
+    this.setState({ articles });
+  }
+
+  prev = () => {
+    this.setState((prevState) => {
+      return {
+        ...prevState,
+        currentPage: prevState.currentPage - 1,
+      };
+    });
+  };
+
+  next = () => {
+    this.setState((prevState) => {
+      return {
+        ...prevState,
+        currentPage: prevState.currentPage + 1,
+      };
+    });
+  };
+
+  updateData = async ({ country, category, searchText }) => {
+    const articles = await fetchNews(country, category, searchText);
+    this.setState({ articles, currentPage: 1 });
+  };
+
+  render() {
+    const { currentPage, articles } = this.state;
+    return (
+      <div className="container">
+        <NewsFilter updateData={this.updateData} />
+
+        <div className="article-container">
+          <Button disabled={currentPage === 1} onClick={this.prev}>
+            <ArrowBack />
+          </Button>
+          {articles.length !== 0 ? (
+            <NewsDisplay currentNews={articles[currentPage - 1]} />
+          ) : (
+            <CircularProgress />
+          )}
+          <Button
+            disabled={currentPage === articles.length}
+            onClick={this.next}
+          >
+            <ArrowForward />
+          </Button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
